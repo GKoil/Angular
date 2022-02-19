6539a50891730400e7f69d70fa9e50d3482cd0f9
Kirill <samsuu@ya.ru>
2022-02-19 12:22:30 +0300

    feature add processingCar

--- src/app/pages/home/components/list-cars/list-cars.component.ts
+++ src/app/pages/home/components/list-cars/list-cars.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CarsService } from '@services/cars.service';
 import { Car } from '@services/car.type';
+import { Constants } from '@app/constants';
 
 @Component({
   selector: 'app-list-cars',
   templateUrl: './list-cars.component.html',
 })
 export class ListCarsComponent implements OnInit {
+  processingCar = ({
+    id,
+    name = '',
+    color = '',
+    image = '',
+    year,
+    model,
+  }: Car) => ({
+    id,
+    name,
+    color,
+    image: `${Constants.URL}/images/${image}`,
+    year,
+    model,
+  });
+
   cars: Car[] = [];
 
   constructor(private carsService: CarsService) {}
 
   ngOnInit() {
     const newCars = this.carsService.getCars();
-    newCars.subscribe((response) => (this.cars = response));
+    newCars.subscribe(
+      (response) => (this.cars = response.map(this.processingCar))
+    );
   }
 
   removeCar(carId: number) {
