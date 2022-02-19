bde1b1ef97dc83254f386f43f5319581b774e7b9
Kirill <samsuu@ya.ru>
2022-02-19 11:23:13 +0300

    refactor remove scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { CarsService } from './services/cars.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   cars: any[] = [];
