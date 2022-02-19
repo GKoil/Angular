ecd08dd9a0677e56aedf3c407fb45fdec2f843a9
Кирилл <samsuu@ya.ru>
2022-02-19 14:11:00 +0300

    refactor home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { CarsService } from '@services/cars.service';
-import { Car } from '@services/car.type';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
-  cars: Car[] = [];
-
+export class HomeComponent {
   contentTitle = 'Cars List';
-
-  constructor(private carsService: CarsService) {}
-
-  ngOnInit() {
-    const newCars = this.carsService.getCars();
-    newCars.subscribe((response) => (this.cars = response));
-  }
-
-  removeCar(carId: number) {
-    this.cars = this.cars.filter(({ id }: any) => id !== carId);
-  }
 }
